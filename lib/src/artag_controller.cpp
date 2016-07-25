ea93c5ad717611c4d4c3d750d861191041ec9e91
Alessandro Roncone <alecive87@gmail.com>
2016-07-25 14:12:28 -0400

    Updated code to reflect change in TTT API

--- lib/src/artag_controller.cpp
+++ lib/src/artag_controller.cpp
@@ -111,7 +111,8 @@ bool ARTagController::pickARTag()
 
 void ARTagController::goToPose(geometry_msgs::PoseStamped req_pose_stamped)
 {
-    vector<double> joint_angles = getJointAngles(req_pose_stamped);
+    vector<double> joint_angles;
+    getJointAngles(req_pose_stamped,joint_angles);
 
     baxter_core_msgs::JointCommand joint_cmd;
     joint_cmd.mode = baxter_core_msgs::JointCommand::POSITION_MODE;
