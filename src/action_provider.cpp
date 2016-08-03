5ec52e1567d8f9e6b3f42357becafcac27a2db5f
Alessandro Roncone <alecive87@gmail.com>
2016-08-03 15:35:28 -0400

    Surfaced API changes to the action provider. Removed actionProvider class accordingly (it is not needed anymore because everything is wrapped into ARMCtrl)

--- src/action_provider.cpp
+++ src/action_provider.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 #include <ros/ros.h>
-
-#include "actionProvider.h"
+#include "robot_interface/artag_ctrl.h"
+#include "robot_interface/hold_ctrl.h"
 
 using namespace std;
 
@@ -10,7 +10,8 @@ int main(int argc, char ** argv)
 {
     ros::init(argc, argv, "action_provider");
 
-    actionProvider ap("action_provider");
+    ARTagCtrl  left_ctrl("action_provider","left");
+    HoldCtrl  right_ctrl("action_provider","right");
 
     printf("\n");
     ROS_INFO("READY! Waiting for a service message..\n");
