0d90bf1cfe4cdc04fe70f8c9adf251ba20b5881c
Alessandro Roncone <alecive87@gmail.com>
2016-08-05 10:05:20 -0400

    Minor

--- lib/src/hold_ctrl.cpp
+++ lib/src/hold_ctrl.cpp
@@ -111,14 +111,12 @@ bool HoldCtrl::serviceOtherLimbCb(baxter_collaboration::AskFeedback::Request  &r
 
 bool HoldCtrl::prepare4HandOver()
 {
-    return ROSThread::goToPose(0.65, 0.1, Z_LOW,
-                                  HANDOVER_ORI_R);
+    return goToPose(0.65, 0.1, Z_LOW, HANDOVER_ORI_R);
 }
 
 bool HoldCtrl::goHoldPose(double height)
 {
-    return ROSThread::goToPose(0.80, -0.4, height,
-                                HORIZONTAL_ORI_R);
+    return goToPose(0.80, -0.4, height, HORIZONTAL_ORI_R);
 }
 
 HoldCtrl::~HoldCtrl()
