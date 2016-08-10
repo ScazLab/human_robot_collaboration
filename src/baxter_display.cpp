b3490936c78773bf2887c437f02c6166502313b6
Alessandro Roncone <alecive87@gmail.com>
2016-08-10 13:27:33 -0400

    [baxterDisplay] minor fix in missing library

--- src/baxter_display.cpp
+++ src/baxter_display.cpp
@@ -4,6 +4,7 @@
 #include <ros/console.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
 
 #include "baxter_collaboration/ArmState.h"
 
