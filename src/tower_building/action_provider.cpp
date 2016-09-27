4916becbbe9b015e91498267935247094fd0d405
Alessandro Roncone <alecive87@gmail.com>
2016-09-27 15:22:41 -0400

    [tower_building] Added prototype for tower building action provider

---
+++ src/tower_building/action_provider.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <ros/ros.h>
+#include "tower_building/cube_picker.h"
+// #include "flatpack_furniture/hold_ctrl.h"
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    ros::init(argc, argv, "action_provider");
+    ros::NodeHandle _n("action_provider");
+
+    bool use_robot;
+    _n.param<bool>("use_robot", use_robot, true);
+    printf("\n");
+    ROS_INFO("use_robot flag set to %s", use_robot==true?"true":"false");
+
+    printf("\n");
+    CubePicker  left_ctrl("action_provider","left", !use_robot);
+    printf("\n");
+    // HoldCtrl  right_ctrl("action_provider","right", !use_robot);
+    printf("\n");
+    ROS_INFO("READY! Waiting for service messages..\n");
+
+    ros::spin();
+    return 0;
+}
+
