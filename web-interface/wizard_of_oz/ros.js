613715fe08ef877406d91fefd52bdc9f6460cccd
Alessandro Roncone <alecive87@gmail.com>
2016-09-30 22:38:25 -0400

    Added web interface for wizard of oz. Still missing the ROS layer, though

---
+++ web-interface/wizard_of_oz/ros.js
@@ -0,0 +1,212 @@
+// Connecting to ROS
+// -----------------
+//
+var beginning = Date.now();
+var ros = new ROSLIB.Ros();
+
+function resetDate() {
+  beginning = Date.now();
+}
+
+// If there is an error on the backend, an 'error' emit will be emitted.
+ros.on('error', function(error) {
+  document.getElementById('connecting').style.display = 'none';
+  document.getElementById('connected').style.display = 'none';
+  document.getElementById('closed').style.display = 'none';
+  document.getElementById('error').style.display = 'block';
+  document.getElementById('troubleshooting').style.display = 'inline-block';
+  console.log(error);
+});
+
+// Find out exactly when we made a connection.
+ros.on('connection', function() {
+  console.log('Connection made!');
+  document.getElementById('connecting').style.display = 'none';
+  document.getElementById('error').style.display = 'none';
+  document.getElementById('closed').style.display = 'none';
+  document.getElementById('connected').style.display = 'block';
+});
+
+ros.on('close', function() {
+  console.log('Connection closed.');
+  document.getElementById('connecting').style.display = 'none';
+  document.getElementById('connected').style.display = 'none';
+  document.getElementById('closed').style.display = 'inline-block';
+  document.getElementById('error').style.display = 'inline-block';
+});
+
+// Guess connection of the rosbridge websocket
+function getRosBridgeHost() {
+  if (window.location.protocol == 'file:') {
+    return 'localhost';
+  } else {
+    return window.location.hostname;
+  }
+}
+
+var rosBridgePort = 9090;
+// Create a connection to the rosbridge WebSocket server.
+ros.connect('ws://' + getRosBridgeHost() + ':' + rosBridgePort);
+
+// First, we create a Topic object with details of the topic's name and message type.
+var logTopic = new ROSLIB.Topic({
+  ros : ros,
+  name : '/web_interface/log',
+  messageType : 'std_msgs/String'
+});
+
+// First, we create a Topic object with details of the topic's name and message type.
+var webInterfacePub = new ROSLIB.Topic({
+  ros : ros,
+  name : '/web_interface/pub',
+  messageType : 'std_msgs/String'
+});
+
+// Topic for error passing to the left arm
+var errorPressedL = new ROSLIB.Topic({
+  ros : ros,
+  name : '/robot/digital_io/left_lower_button/state',
+  messageType : 'baxter_core_msgs/DigitalIOState'
+});
+
+// Topic for error passing to the right arm
+var errorPressedR = new ROSLIB.Topic({
+  ros : ros,
+  name : '/robot/digital_io/right_lower_button/state',
+  messageType : 'baxter_core_msgs/DigitalIOState'
+});
+
+// Service Client to interface with the left arm
+var leftArmService  = new ROSLIB.Service({
+  ros : ros,
+  name: '/action_provider/service_left',
+  messageType : 'baxter_collaboration/DoAction'
+});
+
+// Service Client to interface with the right arm
+var rightArmService = new ROSLIB.Service({
+  ros : ros,
+  name: '/action_provider/service_right',
+  messageType : 'baxter_collaboration/DoAction'
+});
+
+var webInterfaceSub = new ROSLIB.Topic({
+  ros : ros,
+  name: '/web_interface/sub',
+  messageType: 'std_msgs/String'
+});
+
+webInterfaceSub.subscribe(function(msg) {
+  console.log('Received message on ' + webInterfaceSub.name);
+  var data = JSON.parse(msg.data);
+  updatetowerscheme(data.towers);
+});
+
+// Add a callback for any element on the page
+function callback(e) {
+    var e = window.e || e;
+
+    // console.log(e.target.tagName);
+    if (e.target.tagName == 'BUTTON')
+    {
+        var obj = e.target.firstChild.nodeValue;
+        console.log('Pressed '+ e.target.tagName +
+                    ' item: ' + obj);
+
+        if (obj == 'error')
+        {
+          var message = new ROSLIB.Message({
+            state: 1,
+            isInputOnly: true
+          });
+
+          errorPressedL.publish(message);
+          errorPressedR.publish(message);
+        }
+        else if (obj == 'hold' || obj == 'release')
+        {
+          var req = new ROSLIB.ServiceRequest();
+          req.object = -1;
+
+          if      (obj == 'hold')    { req.action = 'start_hold'; }
+          else if (obj == 'release') { req.action =   'end_hold'; }
+
+          var res = new ROSLIB.ServiceResponse();
+
+          console.log('Requested: ', req.action, req.object);
+          rightArmService.callService(req,function(res)
+          {
+              console.log('Got Response: ' + res.success);
+          });
+        }
+        else if (obj == 'get CF' || obj == 'get LL'  ||
+                 obj == 'get RL' || obj == 'get TOP' ||
+                 obj == 'pass' )
+        {
+          var req = new ROSLIB.ServiceRequest();
+
+          if (obj == 'pass') { req.action = 'pass'; }
+          else               { req.action = 'get';  }
+
+          if      (obj.replace('get ','') == 'CF')  { req.object = 24; }
+          else if (obj.replace('get ','') == 'LL')  { req.object = 17; }
+          else if (obj.replace('get ','') == 'RL')  { req.object = 26; }
+          else if (obj.replace('get ','') == 'TOP') { req.object = 21; }
+          else { console.error('Requested an object that was not allowed!'); };
+
+          var res = new ROSLIB.ServiceResponse();
+
+          console.log('Requested: ', req.action, req.object);
+          leftArmService.callService(req,function(res)
+          {
+            console.log('Got Response: ' + res.success);
+          });
+        }
+        else if (obj == 'home')
+        {
+          var req = new ROSLIB.ServiceRequest();
+          var res = new ROSLIB.ServiceResponse();
+          req.action = obj;
+          req.object =  -1;
+
+          console.log('Requested: ', req.action, req.object);
+          rightArmService.callService(req,function(res)
+          {
+              console.log('[right] Got Response: ' + res.success);
+          });
+          leftArmService.callService(req,function(res)
+          {
+              console.log('[left] Got Response: ' + res.success);
+          });
+        }
+        else
+        {
+          if (obj == 'finish') obj = 'stop'; // To comply with the py code
+
+          var message = new ROSLIB.Message({
+            data: obj
+          });
+
+          webInterfacePub.publish(message);
+        }
+
+        var datenow = Date.now();
+        var elapsed = (datenow - beginning)/1000;
+        var timestamp = '[' + datenow + '][' + elapsed + ']';
+        var logstring = timestamp + ' ' + obj;
+
+        // console.log(logstring);
+        var message = new ROSLIB.Message({
+          data: logstring
+        });
+        logTopic.publish(message);
+    }
+
+    return;
+}
+
+if (document.addEventListener)
+    document.addEventListener('click', callback, false);
+else
+    document.attachEvent('onclick', callback);
+
---
+++ web-interface/wizard_of_oz/wizard_of_oz.js
@@ -0,0 +1,88 @@
+var defaultjsonfile = 'woz.json';
+
+loadtowerscheme('');
+
+var vis;
+var draw;
+
+var i = 0,
+    rectW = 140,
+    rectH = 140;
+
+function loadtowerscheme(file)
+{
+  if (file == '') { file = defaultjsonfile;}
+  else            { defaultjsonfile = file;};
+
+  console.log('Loading file',file);
+
+  var width  =  900,
+      height =  450;
+
+  var svg = d3.select('svg')
+              //responsive SVG needs these 2 attributes and no width and height attr
+              .attr('preserveAspectRatio', 'xMinYMin meet')
+              .attr('viewBox', '0 0 ' + width + ' ' + height)
+              //class to make it responsive
+              .classed('svg-content-responsive', true);
+
+  svg.call(d3.behavior.zoom().scaleExtent([0.4, 3]).on('zoom', redraw));
+
+  vis  = svg.append('svg:g');
+  draw = vis.append('svg:g')
+            .attr('transform', 'translate(' + 140 + ',' + 60 + ')');
+
+  // load the external data
+  d3.json('json/'+file, function(error, json)
+  {
+    if (error) {throw error;}
+    updatetowerscheme(json.towers);
+  });
+};
+
+function updatetowerscheme(source) {
+  draw.selectAll('*').remove();
+  console.log('Updating towers..',source);
+
+  var towers = draw.selectAll('g')
+                   .data(source)
+                   .enter()
+                   .append('g')
+                   .attr('class', 'tower')
+                   .attr('transform', function(d,i) { return 'translate(' + d.x0*(rectW+100) + ',' + 0 + ')'; });
+
+  var max_length = 0;
+  towers.each(function(d,i) {
+    if (d.blocks.length>max_length) { max_length=d.blocks.length}
+  });
+
+  towers.each(function(d,i) {
+    for (var j = 0; j < max_length; j++)
+    {
+      if (j<d.blocks.length) {
+      var block = d3.select(this)
+                    .append('g')
+                    .attr('transform', 'translate(' + 0 + ',' + ((max_length-1-j)*rectH*(1+0.04)) + ')');
+
+      block.append('rect')
+           .attr('width', rectW)
+           .attr('height', rectH)
+           .attr('class', 'label')
+           .style('fill', d.blocks[j].color);
+
+      block.append('text')
+           .attr('x', rectW / 2)
+           .attr('y', rectH / 2)
+           .attr('text-anchor', 'middle')
+           .text(d.blocks[j].label);
+      }
+    }
+  });
+};
+
+//Redraw for zoom
+function redraw() {
+  vis.attr('transform',
+           'translate(' + d3.event.translate + ')'
+            + ' scale(' + d3.event.scale + ')');
+};
