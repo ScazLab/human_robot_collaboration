b41309012cf63e2385d43df8c49e4747f6ec9850
Alessandro Roncone <alecive87@gmail.com>
2016-12-01 15:45:44 -0500

    Added another small test for the dot product

--- test/test_utils_lib.cpp
+++ test/test_utils_lib.cpp
@@ -34,6 +34,15 @@ TEST(UtilsLib, geometry_msgsPointsOperators)
     // Test dot product
     EXPECT_EQ(dot(a,a)/(norm(a)*norm(a)),1);
     EXPECT_EQ(dot(a,a*-1)/(norm(a)*norm(a)),-1);
+
+    geometry_msgs::Point o, d, c;
+    o.x = 0; o.y =  0; o.z =  0;
+    d.x = 4; d.y = 12; d.z = 18;
+
+    c = d/2;
+    EXPECT_EQ(dot(d-o, d-c)/(norm(d-o)*norm(d-c)), +1);
+    c = d*2;
+    EXPECT_EQ(dot(d-o, d-c)/(norm(d-o)*norm(d-c)), -1);
 }
 
 // Run all the tests that were declared with TEST()
