e35bd71b1589c4c9053ad8f05930f985584f46b3
Alessandro Roncone <alecive87@gmail.com>
2017-03-23 19:10:52 -0400

    Changed name to publishJointCmd

--- baxter_collaboration_lib/src/robot_interface/robot_interface.cpp
+++ baxter_collaboration_lib/src/robot_interface/robot_interface.cpp
@@ -518,7 +518,7 @@ bool RobotInterface::goToJointConfNoCheck(vector<double> joint_values)
         joint_cmd.command.push_back(joint_values[i]);
     }
 
-    publish_joint_cmd(joint_cmd);
+    publishJointCmd(joint_cmd);
 
     return true;
 }
@@ -934,7 +934,7 @@ void RobotInterface::setState(int state)
     return;
 }
 
-void RobotInterface::publish_joint_cmd(baxter_core_msgs::JointCommand _cmd)
+void RobotInterface::publishJointCmd(baxter_core_msgs::JointCommand _cmd)
 {
     _joint_cmd_pub.publish(_cmd);
 }
