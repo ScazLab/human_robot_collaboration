6cd1e891a49adfe7a889997ad2f8b45391ed2c58
akrzywosz <andrew.krzywosz@yale.edu>
2017-06-06 13:43:22 -0400

    Added rosthreadimage test

---
+++ baxter_collaboration_lib/test/test_ros_thread_image.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include "robot_utils/ros_thread_image.h"
+
+
+class ROSImageObject: public ROSThreadImage
+{
+public:    
+    ROSImageObject(std::string namenew): ROSThreadImage(namenew) {}
+    void InternalThreadEntry(); 
+};
+
+
+void ROSImageObject::InternalThreadEntry() {
+    std::cout << "test" << std::endl;
+};
+
+
+TEST(rosimagetest, testsetname)
+{
+   ROSImageObject rt("test");
+   rt.joinInternalThread(); //
+
+   rt.setName("newtestname");
+   EXPECT_EQ("newtestname", rt.getName());
+   EXPECT_FALSE("test" == rt.getName()); 
+}
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "ros_thread_test"); 
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
