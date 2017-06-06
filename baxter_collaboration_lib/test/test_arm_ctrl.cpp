e09a050490b6d9232e8b2c90c82283846abdd9b0
akrzywosz <andrew.krzywosz@yale.edu>
2017-06-06 16:06:24 -0400

    Changed file names to test_arm_ctrl , fix code cosmetic issues, removed catkin libraries dependency for test_arm_ctrl in cmake

--- baxter_collaboration_lib/test/test_arm_ctrl.cpp
+++ baxter_collaboration_lib/test/test_arm_ctrl.cpp
@@ -3,20 +3,18 @@
 
 using namespace std;
 
-
-    //Potentially expand on the testcases; check to see that all edge values have been covered
-
 TEST(ArmControlTest, testConstructorDefaultValues)
 {
     std::string _name = "robot"; 
-    std::string _limb = "left";
-    bool _use_forces = true; 
-    //Since we are actually not using the robot, _use_robot needs to be set to 'false' even though by default it should be true
+    std::string _limb =  "left";
+    bool  _use_forces =    true; 
+    //Since we are actually not using the robot, _use_robot 
+    //needs to be set to 'false' even though by default it should be true
     //Setting this to true yields a KDL chain error
     //Perhaps this could be tested using a simulation?
-    bool _use_robot = false; 
-    bool _use_trac_ik = true;
-    bool _use_cart_ctrl = false;
+    bool     _use_robot  = false; 
+    bool   _use_trac_ik  =  true;
+    bool _use_cart_ctrl  = false;
 
     ArmCtrl ac (_name, _limb, _use_robot, _use_forces, _use_trac_ik, _use_cart_ctrl);
 
@@ -32,13 +30,14 @@ TEST(ArmControlTest, testConstructorDefaultValues)
 
 TEST(ArmControlTest, testConstructorCustomValues) 
 {
-    std::string _name = "robot"; 
-    std::string _limb = "left";
-    //overrides default constructor values for these variables; these values are the opposite of the default values bool _use_forces = false; 
-    bool _use_forces = false;
-    bool _use_robot = false;      
-    bool _use_trac_ik = false;
-    bool _use_cart_ctrl = true; 
+    std::string _name =  "robot"; 
+    std::string _limb =   "left";
+    //overrides default constructor values for these variables; these values 
+    //are the opposite of the default values bool _use_forces = false; 
+    bool    _use_forces =  false;
+    bool     _use_robot =  false;      
+    bool   _use_trac_ik =  false;
+    bool _use_cart_ctrl =   true; 
 
     ArmCtrl ac (_name, _limb, _use_robot, _use_forces, _use_trac_ik, _use_cart_ctrl);
 
@@ -54,34 +53,36 @@ TEST(ArmControlTest, testConstructorCustomValues)
 TEST(ArmControlTest, testPublicSetterGetterMethods)
 {
     std::string _name = "robot"; 
-    std::string _limb = "left";
-    //overrides default constructor values for these variables; these values are the opposite of the default values
-    bool _use_forces = true; 
-    bool _use_robot = false;      
-    bool _use_trac_ik = true;
+    std::string _limb =  "left";
+    //overrides default constructor values for these variables; these
+    // values are the opposite of the default values
+    bool    _use_forces =  true; 
+    bool     _use_robot = false;      
+    bool   _use_trac_ik =  true;
     bool _use_cart_ctrl = false;
 
     ArmCtrl ac (_name, _limb, _use_robot, _use_forces, _use_trac_ik, _use_cart_ctrl);
 
     //Setter Methods
     std::string _state = "teststate";
-    int _obj = 5; //some random obj id number
+    int _obj = 5;               //some random obj id number
     vector<int> _objs (4, 100); //some random value vector
-    std::string _action = "testaction";
+    
+    std::string      _action =     "testaction";
     std::string _prev_action = "testprevaction";
 
-    ac.setSubState(_state);
-    ac.setObjectID(_obj);
-    ac.setObjectIDs(_objs);
-    ac.setAction(_action);
+    ac.setSubState(       _state);
+    ac.setObjectID(         _obj);
+    ac.setObjectIDs(       _objs);
+    ac.setAction(        _action);
     ac.setPrevAction(_prev_action);
 
     //Getter methods
-    EXPECT_EQ("teststate", ac.getSubState()); 
-    EXPECT_EQ("testaction", ac.getAction());  
+    EXPECT_EQ(     "teststate", ac.getSubState()); 
+    EXPECT_EQ(    "testaction", ac.getAction());  
     EXPECT_EQ("testprevaction", ac.getPrevAction());  
-    EXPECT_EQ(_obj, ac.getObjectID());
-    EXPECT_EQ(_objs, ac.getObjectIDs());
+    EXPECT_EQ(            _obj, ac.getObjectID());
+    EXPECT_EQ(           _objs, ac.getObjectIDs());
 }
 
 // Run all the tests that were declared with TEST()
