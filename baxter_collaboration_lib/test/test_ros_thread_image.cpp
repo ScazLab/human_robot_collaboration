10a52dcc328e69892754d08b168d54faa140329f
Alessandro Roncone <alecive87@gmail.com>
2017-06-07 16:07:48 -0400

    [test] Cosmetic

--- baxter_collaboration_lib/test/test_ros_thread_image.cpp
+++ baxter_collaboration_lib/test/test_ros_thread_image.cpp
@@ -4,14 +4,12 @@
 
 class ROSImageObject: public ROSThreadImage
 {
-public:    
+public:
     explicit ROSImageObject(std::string namenew): ROSThreadImage(namenew) {}
-    void InternalThreadEntry(); 
-};
-
-
-void ROSImageObject::InternalThreadEntry() {
-    std::cout << "test" << std::endl;
+    void InternalThreadEntry()
+    {
+        std::cout << "test" << std::endl;
+    }
 };
 
 
@@ -22,13 +20,13 @@ TEST(rosimagetest, testsetname)
 
    rt.setName("newtestname");
    EXPECT_EQ("newtestname", rt.getName());
-   EXPECT_FALSE("test" == rt.getName()); 
+   EXPECT_FALSE("test" == rt.getName());
 }
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "ros_thread_test"); 
+    ros::init(argc, argv, "ros_thread_test");
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
