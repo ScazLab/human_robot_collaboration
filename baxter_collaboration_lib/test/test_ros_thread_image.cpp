f2b89b75e81551ba3c17e55c8c647c06185c2695
akrzywosz <andrew.krzywosz@yale.edu>
2017-06-14 11:36:03 -0400

    Fixed pull request issues

--- baxter_collaboration_lib/test/test_ros_thread_image.cpp
+++ baxter_collaboration_lib/test/test_ros_thread_image.cpp
@@ -7,45 +7,42 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-static const std::string OPENCV_WINDOW = "Image window";
-
 using namespace cv;
 using namespace std;
 
 class ROSThreadImageTester
 {
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Publisher image_pub_;
-  std::string name;
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Publisher image_pub_;
+    std::string name;
 
 public:
-  explicit ROSThreadImageTester(std::string _name)
+    explicit ROSThreadImageTester(std::string _name)
     : it_(nh_), name(_name)
-  {
-   image_pub_ = it_.advertise("/"+name+"/image", 1);
-  }
+    {
+        image_pub_ = it_.advertise("/"+name+"/image", 1);
+    }
 
-  ~ROSThreadImageTester() {}
+    ~ROSThreadImageTester() {}
 
-  void sendTestImage()
-  {
-    cv::Mat img(200, 200, CV_8UC3, cv::Scalar(0,0,0));
-    cv::waitKey(30);
-    cv::circle(img, cv::Point(100, 100), 20, CV_RGB(255,0,0), -1); 
-    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
-    image_pub_.publish(msg); 
-  }
+    void sendTestImage()
+    {
+        cv::Mat img(200, 200, CV_8UC3, cv::Scalar(0,0,0));
+        cv::waitKey(30);
+        cv::circle(img, cv::Point(100, 100), 20, CV_RGB(255,0,0), -1); 
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
+        image_pub_.publish(msg); 
+    }
 };
 
 class ROSImageInstance: public ROSThreadImage
 {
 private:
-  int xcoord;
-  int ycoord;
+    Point avg_coords = Point(-1, -1);
 
 public:
-    explicit ROSImageInstance(std::string namenew): ROSThreadImage(namenew), xcoord(-1), ycoord(-1) {}
+    explicit ROSImageInstance(std::string namenew): ROSThreadImage(namenew) {}
     
     void InternalThreadEntry()
     {
@@ -59,7 +56,6 @@ public:
                 pthread_mutex_lock(&_mutex_img);
                 img_in=_curr_img;
                 pthread_mutex_unlock(&_mutex_img);
-
                 cv::Mat gray;
                 cv::cvtColor(img_in, gray, CV_BGR2GRAY);
                 std::vector<vector<Point>> contours;
@@ -67,59 +63,59 @@ public:
 
                 //Finds the moments
                 vector<Moments> mu(contours.size() );
-                for(unsigned int i = 0; i < contours.size(); i++ )
+                for(size_t i = 0; i < contours.size(); i++ )
                 {
                     mu[i] = moments(contours[i], false );
                 }
 
                 //Finds the Centroid
-                vector<Point2f> mc( contours.size());
-                for( unsigned int i = 0; i < contours.size(); i++ )
+                vector<Point2f> mc(contours.size());
+                for( size_t i = 0; i < contours.size(); i++ )
                 { 
-                    mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
-                    xcoord = mu[i].m10/mu[i].m00;
-                    ycoord = mu[i].m01/mu[i].m00;
+                    mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); 
+                    avg_coords.x = mu[i].m10/mu[i].m00;
+                    avg_coords.y = mu[i].m01/mu[i].m00;
                 }
 
                 break;
             }
             r.sleep();
         } 
-      }
-    int getx() {return xcoord;}
-    int gety() {return ycoord;}
+    }
 
-    void printx() {std::cout<<"The calculated x-coord of the centroid of the cirlce is " << xcoord<< " [Expected value: 100]"<<std::endl;}
-    void printy() {std::cout<<"The calculated y-coord of the centroid of the circle is " << ycoord<< " [Expected value: 100]"<<std::endl;}
+//Self explaining return methods  
+int returnx() { return avg_coords.x; }
+
+int returny() { return avg_coords.y; }
+
+Point returnpoint() {return avg_coords; }
+    
 };
 
 
-TEST(rosimagetest, testsetname)
+TEST(rosimagetest, testinternalthreadentry)
 {
+    //Creates an object that sends a 200x200 black image with a red circle overlaid
     ROSThreadImageTester rtit("test");
+    
+    //Creates an object responsible for receiving an image 
+    //and finding the centroid of a contour
     ROSImageInstance rtii("test");
 
+    //Sends a 200x200 image with a red circle overlayed in the middle
     rtit.sendTestImage();
+    
+    //Waits so that threads can finish
     ros::Rate rate(100);
 
-    while(ros::ok() && (rtii.getx() == -1 || rtii.gety() == -1))
+    while(ros::ok() && (rtii.returnx() == -1 || rtii.returny() == -1))
     {
-        ROS_INFO("Waiting for ROSThreadImage loop. [x y]: [%i %i]", rtii.getx(), rtii.gety());
+        //ROS_INFO("Waiting for ROSThreadImage loop. [x y]: [%i %i]", rtii.getx(), rtii.gety());
         rate.sleep();
     }
 
-    rtii.printx();
-    rtii.printy();
-
-    rtii.setName("newtestname");
-    
-    //Checks that x and y coordinates are correct
-    EXPECT_TRUE(rtii.getx() == 100);
-    EXPECT_TRUE(rtii.gety() == 100); 
-
-    //Checks that name is correct
-    EXPECT_EQ("newtestname", rtii.getName());
-    EXPECT_FALSE("test" == rtii.getName());
+    //Checks that x and y coordinates are correct against expected values
+    EXPECT_EQ(rtii.returnpoint(), Point(100, 100));
 }
 
 // Run all the tests that were declared with TEST()
