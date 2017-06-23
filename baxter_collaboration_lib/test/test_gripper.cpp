1b349962f0e8bda81d50cb770970f2c97214a17a
akrzywosz <andrew.krzywosz@yale.edu>
2017-06-23 14:53:45 -0400

    Added Calibration test

--- baxter_collaboration_lib/test/test_gripper.cpp
+++ baxter_collaboration_lib/test/test_gripper.cpp
@@ -38,17 +38,48 @@ public:
 
     void commandCb(const baxter_core_msgs::EndEffectorCommand &msg)
     {
-    
+       // std::string _msg = &msg.command;
+       // std::string _cmd;
+
+
+        baxter_core_msgs::EndEffectorState state;
+        
+        state.calibrated = baxter_core_msgs::EndEffectorState::STATE_TRUE;
+        state.enabled    = baxter_core_msgs::EndEffectorState::STATE_UNKNOWN;
+        state.error      = baxter_core_msgs::EndEffectorState::STATE_UNKNOWN;
+        state.gripping   = baxter_core_msgs::EndEffectorState::STATE_UNKNOWN;
+        state.missed     = baxter_core_msgs::EndEffectorState::STATE_UNKNOWN;
+        state.ready      = baxter_core_msgs::EndEffectorState::STATE_UNKNOWN;
+        state.moving     = baxter_core_msgs::EndEffectorState::STATE_UNKNOWN;
+       
+
+        baxter_core_msgs::EndEffectorCommand _cmd;
+        _cmd.command = baxter_core_msgs::EndEffectorCommand::CMD_CALIBRATE;
+
+        if (msg.command == _cmd.command)
+        {
+            state.calibrated = baxter_core_msgs::EndEffectorState::STATE_TRUE;
+        } 
+
+        state_pub.publish(state);
     }
 
-    // Publishes properties of the gripper
-    void sendProperties()
+    // Publishes properties of the suction cup gripper
+    void sendPropertiesSuction()
     {
         //publish the properties then make sure they are correctly set in the tests
         //std::lock_guard<std::mutex> lock(mutex_properties);
         baxter_core_msgs::EndEffectorProperties prop;
         prop.ui_type = 1u;
         prop_pub.publish(prop);   
+    }
+
+    // Publishes the properties of the electric gripper
+    void sendPropertiesElectric()
+    {
+        baxter_core_msgs::EndEffectorProperties prop;
+        prop.ui_type = 2u;
+        prop_pub.publish(prop); 
     }
 
     // Publishes the state of the gripper
@@ -69,30 +100,26 @@ public:
     }
 };
 
-TEST(GripperTest, testConstructorDefaultValues)
+TEST(GripperTest, testPropertiesAndStateSubscriber)
 {
     std::string    limb = "left";
     bool use_robot =  true;
 
     gripperTester gt("gripper" , "left");
-    gt.sendProperties();
+    gt.sendPropertiesSuction();
     gt.sendState();
-    
-    //ros::Duration(2).sleep();
 
     Gripper gr(limb, use_robot);
 
-    EXPECT_FALSE(gr.is_enabled());
-    EXPECT_FALSE(gr.is_calibrated());
-    EXPECT_FALSE(gr.is_ready_to_grip());
-    EXPECT_FALSE(gr.has_error());
-
-    EXPECT_TRUE(gr.is_sucking());
-    EXPECT_TRUE(gr.is_gripping());
-
+    EXPECT_TRUE(      gr.is_enabled());
+    EXPECT_TRUE(   gr.is_calibrated());  
+    EXPECT_TRUE(gr.is_ready_to_grip());
+    EXPECT_TRUE(      gr.is_sucking());
+    EXPECT_TRUE(     gr.is_gripping());
+    EXPECT_FALSE(      gr.has_error());
+    
     EXPECT_EQ("left", gr.getGripperLimb());
 
-/*
     baxter_core_msgs::EndEffectorState _state = gr.getGripperState();
 
     EXPECT_EQ(_state.calibrated , baxter_core_msgs::EndEffectorState::STATE_TRUE);
@@ -101,29 +128,27 @@ TEST(GripperTest, testConstructorDefaultValues)
     EXPECT_EQ(_state.gripping   , baxter_core_msgs::EndEffectorState::STATE_FALSE);
     EXPECT_EQ(_state.missed     , baxter_core_msgs::EndEffectorState::STATE_FALSE);
     EXPECT_EQ(_state.ready      , baxter_core_msgs::EndEffectorState::STATE_TRUE);
-    EXPECT_EQ(_state.moving     , baxter_core_msgs::EndEffectorState::STATE_TRUE);
-    */
+    EXPECT_EQ(_state.moving     , baxter_core_msgs::EndEffectorState::STATE_TRUE);    
 }
 
-// TEST(GripperTest, testDefaultStates)
-// { 
-//     std::string    limb = "left";
-//     bool use_robot =  true;
+TEST(GripperTest, testDefaultStates)
+{ 
+   std::string    limb = "left";
+   bool use_robot =  true;
 
-//     Gripper gr(limb, use_robot);
+   Gripper gr(limb, use_robot);
+   baxter_core_msgs::EndEffectorState _state;
+   _state = gr.getGripperState();
 
-//      baxter_core_msgs::EndEffectorState _state;
-//      _state = gr.getGripperState();
-
-//     EXPECT_EQ(_state.calibrated , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
-//     EXPECT_EQ(_state.enabled    , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
-//     EXPECT_EQ(_state.error      , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
-//     EXPECT_EQ(_state.gripping   , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
-//     EXPECT_EQ(_state.missed     , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
-//     EXPECT_EQ(_state.ready      , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
-//     EXPECT_EQ(_state.moving     , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
+  EXPECT_EQ(_state.calibrated , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
+  EXPECT_EQ(_state.enabled    , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
+  EXPECT_EQ(_state.error      , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
+  EXPECT_EQ(_state.gripping   , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
+  EXPECT_EQ(_state.missed     , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
+  EXPECT_EQ(_state.ready      , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
+  EXPECT_EQ(_state.moving     , baxter_core_msgs::EndEffectorState::STATE_UNKNOWN);
     
-// } 
+} 
 
 // TEST(GripperTest, testsetGripperState)
 // { 
@@ -153,26 +178,27 @@ TEST(GripperTest, testConstructorDefaultValues)
 //     EXPECT_EQ(_state.moving     , baxter_core_msgs::EndEffectorState::STATE_TRUE);
 
 // }
+TEST(GripperTest, testCalibration)
+{ 
+   std::string    limb = "left";
+   bool       use_robot =  true;
 
-// TEST(GripperTest, testCalibrate)
-// { /*
-//      // causes trace/breakout ; let's use a rostest to fix this
-//     std::string    limb = "left";
-//     bool use_robot =  true;
+   Gripper gr(limb, use_robot);
+   gripperTester gt("gripper", "left");
 
-//     Gripper gr(limb, use_robot);
+   gt.sendPropertiesElectric();
 
-//     gr.calibrate();
-//     baxter_core_msgs::EndEffectorState _state = gr.getGripperState();
+   gr.calibrate();
+   baxter_core_msgs::EndEffectorState _state = gr.getGripperState();
+
+   EXPECT_TRUE(_state.calibrated == baxter_core_msgs::EndEffectorState::STATE_TRUE);
+
+//   gr.clearCalibration();
+//   _state = gr.getGripperState();
     
-//     EXPECT_EQ(_state.calibrated , baxter_core_msgs::EndEffectorState::STATE_TRUE);
-
-//     gr.clearCalibration();
-//     _state = gr.getGripperState();
+//   EXPECT_EQ(_state.calibrated , baxter_core_msgs::EndEffectorState::STATE_FALSE);
     
-//     EXPECT_EQ(_state.calibrated , baxter_core_msgs::EndEffectorState::STATE_FALSE);
-//     */
-// }
+}
 
 // TEST(GripperTest, testwithgts)
 // {    
