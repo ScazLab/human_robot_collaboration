1742b606007b512a110d6301300338008fa07774
Alessandro Roncone <alecive87@gmail.com>
2017-09-08 14:21:27 -0400

    [part_picker] Moved to single file header with definition and implementation

--- human_robot_collaboration/src/modular_furniture/part_picker.h
+++ human_robot_collaboration/src/modular_furniture/part_picker.h
@@ -25,18 +25,47 @@ protected:
      * [passObject description]
      * @return true/false if success/failure
      */
-    bool passObject();
+    bool passObject()
+    {
+        if (getPrevAction() != ACTION_GET)  return false;
+        if (!moveObjectTowardHuman())       return false;
+        if (!waitForUserCuffUpperFb())      return false;
+
+        if (ClientTemplate<int>::getObjectID() != 200)
+        {
+            if (!goToPose(0.50, 0.93, 0.2, POOL_ORI_L)) return false;
+            ros::Duration(0.25).sleep();
+        }
+
+        if (!open())                        return false;
+        if (!homePoseStrict())              return false;
+
+        return true;
+    };
 
 public:
     /**
      * Constructor
      */
-    PartPicker(std::string _name, std::string _limb, bool _use_robot = true);
+    PartPicker(std::string _name, std::string _limb, bool _use_robot = true) :
+                                         ARTagCtrl(_name, _limb, _use_robot)
+    {
+        setHomeConfiguration();
+
+        removeAction(ACTION_PASS);
+        insertAction(ACTION_PASS, static_cast<f_action>(&PartPicker::passObject));
+
+        printActionDB();
+
+        if (not _use_robot) return;
+
+        if (!callAction(ACTION_HOME)) setState(ERROR);
+    };
 
     /**
      * Destructor
      */
-    ~PartPicker();
+    ~PartPicker() { };
 };
 
 #endif
